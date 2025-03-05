bf52b6c5e678d8bc42e487251864b6b42b78e7d2
Jeremiii <38002650+Remiih@users.noreply.github.com>
2025-03-06 02:54:44 +0800

    new file:   Models/Event.cs

---
+++ Models/Event.cs
@@ -0,0 +1,11 @@
+namespace EventEase.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Location { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
